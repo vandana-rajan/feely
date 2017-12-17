eb24b5787d869e1b822c3f00a63b097e9f85b548
vladopp <vlado@itgbg.com>
2017-12-18 00:30:22 +0200

    Extend data_preparator to provide sample data and provide decoding for emotion codes.

--- src/data_preparator.py
+++ src/data_preparator.py
@@ -16,10 +16,11 @@ def flat_to_matrix(pixels_string, size=(48,48)):
     pixels_arr = np.array(map(int, pixels_string.split()))
     return pixels_arr.reshape(size)
 
-def _get_data(data_type):
-    df=pd.read_csv('../data/fer2013.csv', sep=',')
+def _get_data(data_type=None, rows_to_read=None):
+    df=pd.read_csv('../data/fer2013.csv', sep=',', nrows=rows_to_read)
 
-    df = df[df.Usage == data_type]
+    if data_type is not None:
+        df = df[df.Usage == data_type]
     df['pixels'] = df.pixels.apply(lambda x: flat_to_matrix(x))
 
     x = np.array([matrix for matrix in df.pixels])
@@ -36,5 +37,10 @@ def get_test_data():
 def get_train_data():
     return _get_data('Training')
 
-def get_emotion_mapping():
-    return {0:'Angry', 1:'Disgust', 2:'Fear', 3:'Happy', 4:'Sad', 5:'Surprise', 6:'Neutral'}
+def get_emotion_from_mapping(emotion_code):
+    code_to_emotion = {0:'Angry', 1:'Disgust', 2:'Fear', 3:'Happy', 4:'Sad', 5:'Surprise', 6:'Neutral'}
+    return code_to_emotion[emotion_code]
+
+def get_sample():
+    return _get_data(rows_to_read=1)
+    
