6a79fc191ae6adcc579f2e37108f0f7f74f15497
vladopp <vlado@itgbg.com>
2017-12-20 01:17:23 +0200

    Improve neural net architecture so now it can achieve arount 55-58% accuracy. Increase the convolution layers to 9. Remove dropouts between convolution layres. Remove one of the dense layers. Add validation data in the fit function.

--- src/trainer.py
+++ src/trainer.py
@@ -9,9 +9,9 @@ TODO:
 from keras.models import Sequential
 from keras.layers import Dense, Dropout, Flatten
 from keras.layers import Conv2D, MaxPooling2D
-from keras.optimizers import SGD
 from data_preparator import get_test_data
 from data_preparator import get_train_data
+from src.data_preparator import get_validation_data
 
 
 def create_model():
@@ -20,46 +20,53 @@ def create_model():
     
     # input: 48x48 images with 1 channel
     # this applies 32 convolution filters of size 3x3 each.
-    model.add(Conv2D(32, (3, 3), activation='relu', input_shape=(48, 48, 1)))
-    model.add(Conv2D(32, (3, 3), activation='relu'))
+    model.add(Conv2D(32, (3, 3), padding='same', activation='relu', input_shape=(48, 48, 1)))
+    model.add(Conv2D(32, (3, 3), padding='same', activation='relu'))
+    model.add(Conv2D(32, (3, 3), padding='same', activation='relu'))
     model.add(MaxPooling2D(pool_size=(2, 2)))
-    model.add(Dropout(0.25))
-    
-    model.add(Conv2D(64, (3, 3), activation='relu'))
-    model.add(Conv2D(64, (3, 3), activation='relu'))
+
+    model.add(Conv2D(64, (3, 3), padding='same', activation='relu'))
+    model.add(Conv2D(64, (3, 3), padding='same', activation='relu'))
+    model.add(Conv2D(64, (3, 3), padding='same', activation='relu'))
     model.add(MaxPooling2D(pool_size=(2, 2)))
-    model.add(Dropout(0.25))
-    
+
+    model.add(Conv2D(128, (3, 3), padding='same', activation='relu'))
+    model.add(Conv2D(128, (3, 3), padding='same', activation='relu'))
+    model.add(Conv2D(128, (3, 3), padding='same', activation='relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+
     model.add(Flatten())
-    model.add(Dense(256, activation='relu'))
-    model.add(Dropout(0.5))
+    model.add(Dense(64, activation='relu'))
+    model.add(Dense(64, activation='relu'))
+    model.add(Dropout(0.2))
     model.add(Dense(7, activation='softmax'))
     
-    sgd = SGD(lr=0.1, decay=1e-6, momentum=0.9, nesterov=True)
-    model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
+    model.compile(loss='categorical_crossentropy', optimizer="adam", metrics=['accuracy'])
     
     return model
 
 
-def save_model(model, save_path):
-    # serialize model to JSON
+def save_model(model, save_path, accuracy):
     model_json = model.to_json()
-    with open(save_path + "model.json", "w") as json_file:
+    with open(save_path + "model-" + repr(accuracy) + ".json", "w") as json_file:
         json_file.write(model_json)
-    # serialize weights to HDF5
-    model.save_weights(save_path + "model.h5")
+    model.save_weights(save_path + "model-" + repr(accuracy) + ".h5")
     print("Saved model to disk")
 
 
 def main():
     model = create_model()
-    
+
     print 'Loading data'
     x_test, y_test = get_test_data()
     x_train, y_train = get_train_data()
+    x_validation, y_validation = get_validation_data()
 
     print 'Starting training'
-    model.fit(x_train, y_train, batch_size=32, epochs=10, verbose=2)
+    try:
+        model.fit(x_train, y_train, validation_data=(x_validation, y_validation), batch_size=32, epochs=30, verbose=1)
+    except KeyboardInterrupt:
+        print 'Training Interrupted'
     
     print 'Evaluating'
     loss_and_accuracy = model.evaluate(x_test, y_test, batch_size=32)
@@ -67,7 +74,7 @@ def main():
     print "Accuracy: ", loss_and_accuracy[1]
     
     print 'Saving model'
-    save_model(model, "../models/")
+    save_model(model, "../models/", loss_and_accuracy[1])
     
 
 if __name__ == "__main__":
